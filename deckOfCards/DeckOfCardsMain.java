2b1fe41e37a09279b0b7f386709086e79838d7fa
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-18 23:11:01 +0530

    OOP program

---
+++ deckOfCards/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package com.company1.Day11AndDay12Problems.deckOfCards;
+
+public class DeckOfCardsMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome In Deck Of Cards Program");
+        DeckOfCards cards = new DeckOfCards();
+        cards.distribute();
+
+    }
+}
